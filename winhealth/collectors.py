e2d34e9be2a0d6f5ee763918b281b20d7a9dc465
Roger Weng <rogerw0303@gmail.com>
2026-06-09 10:15:36 -0400

    Add python data collector module and connect to database

---
+++ winhealth/collectors.py
@@ -0,0 +1,12 @@
+import json
+import subprocess
+
+# a function that will run whatever powershell script we give it and return the output as JSON
+def run_powershell_collector(script_path):
+    result = subprocess.run(
+        ["powershell", "-ExecutionPolicy", "Bypass", "-File", script_path], 
+        capture_output=True, 
+        text=True
+        check=True
+    )
+    return json.loads(result.stdout)
