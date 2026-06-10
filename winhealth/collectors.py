05a89e9184ad2e9227e6d7fca7c8802cb88ecc1f
Roger Weng <rogerw0303@gmail.com>
2026-06-10 11:12:38 -0400

    fixed changes that if a powershell collector failed, it will give a message output of the error and the script path

--- winhealth/collectors.py
+++ winhealth/collectors.py
@@ -1,5 +1,10 @@
 import json
 import subprocess
+from unittest import result
+
+class CollectorError(Exception):
+    """Custom exception for collector errors."""
+    pass
 
 # a function that will run whatever powershell script we give it and return the output as JSON
 def run_powershell_collector(script_path):
@@ -7,6 +12,12 @@ def run_powershell_collector(script_path):
         ["powershell", "-ExecutionPolicy", "Bypass", "-File", script_path], 
         capture_output=True, 
         text=True,
-        check=True
     )
-    return json.loads(result.stdout)
+# if window blocks/crashes the script, we want to catch that and raise a custom error
+    if result.returncode != 0: 
+        raise CollectorError(f"Script failed: {script_path}: {result.stderr.strip()}")
+    
+    try:
+        return json.loads(result.stdout)
+    except json.JSONDecodeError:
+        raise CollectorError(f"Script {script_path} didn't return valid JSON: {result.stdout.strip()}")
