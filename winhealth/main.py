15beb05e7043db2baacb423a400487311f6a77cc
Roger Weng <rogerw0303@gmail.com>
2026-06-10 10:24:56 -0400

    Fixed changes, added not null to pc_name and os_type so that it can't be empty, and check() to cpu usage pct and the ram so that it cant be a unrealistic number

--- winhealth/main.py
+++ winhealth/main.py
@@ -6,8 +6,14 @@ conn = sqlite3.connect("WinHealth.db")
 conn.execute("""
     CREATE TABLE IF NOT EXISTS Snapshots (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        pc_name TEXT, os_type TEXT, cpu_usage_pct REAL,
-        total_ram_gb REAL, free_ram_gb REAL, disk_free_gb REAL,
+        pc_name TEXT NOT NULL, 
+        os_type TEXT NOT NULL, 
+        
+        cpu_usage_pct REAL CHECK(cpu_usage_pct >= 0 AND cpu_usage_pct <= 100),
+        total_ram_gb REAL CHECK(total_ram_gb >= 0), 
+        free_ram_gb REAL CHECK(free_ram_gb >= 0),
+        disk_free_gb REAL CHECK(disk_free_gb >= 0),
+             
         timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
     );
 """)
